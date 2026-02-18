8fa239f6ab1ac420b6561d5824cf6e07991a8aa9
ivolzkm <ivolzkmjr1326@gmail.com>
2026-02-18 17:35:53 -0300

    Ajusta consulta a versão do postgres

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -6,34 +6,14 @@ import database from 'infra/database.js';
 export async function GET(request) {
   const updatedAt = new Date().toISOString();
 
-  const databaseName = process.env.POSTGRES_DB;
-
-  const [
-    databaseVersionResult,
-    databaseMaxConnectionsResult,
-    databaseOpenedConnectionsResult,
-  ] = await Promise.all([
-    database.query('SHOW server_version;'),
-    database.query('SHOW max_connections;'),
-    database.query({
-      text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
-      values: [databaseName],
-    }),
-  ]);
-
+  const databaseVersionResult = await database.query('SHOW server_version;');
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count;
-
+  console.log(databaseVersionValue);
   return NextResponse.json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenedConnectionsValue,
       },
     },
   });
--- tests/integration/api/get.test.js
+++ tests/integration/api/get.test.js
@@ -7,6 +7,9 @@ test('GET to /api/v1/status should return 200', async () => {
   expect(response.status).toBe(200);
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual(
+    '18.1 (Debian 18.1-1.pgdg13+2)',
+  );
 
   new Date(responseBody.updated_at).toISOString(); // Verifica se é uma data válida
   //TODO : verificar se a data é não nula e se é uma data válida
