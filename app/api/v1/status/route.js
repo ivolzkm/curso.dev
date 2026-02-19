3ade8e4bf434df4a568d7149f2bebd966ff389ae
ivolzkm <ivolzkmjr1326@gmail.com>
2026-02-19 11:10:10 -0300

    Adiciona openned connections na responseBody e cria um teste para o mesmo

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -6,18 +6,21 @@ import database from 'infra/database.js';
 export async function GET(request) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query('SHOW server_version;');
+  const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-const databaseMaxConnectionsResult = await database.query('SHOW max_connections;')
+const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
 const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
+const databaseOpenedConnectionsResult = await database.query( "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
+const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
   return NextResponse.json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue)
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue
       },
     },
   });
--- tests/integration/api/get.test.js
+++ tests/integration/api/get.test.js
@@ -14,6 +14,8 @@ test('GET to /api/v1/status should return 200', async () => {
   );
   expect(responseBody.dependencies.database.max_connections).toEqual(100)
 
+  expect(responseBody.dependencies.database.opened_connections).toEqual(0)
+
   new Date(responseBody.updated_at).toISOString(); // Verifica se é uma data válida
   //TODO : verificar se a data é não nula e se é uma data válida
 });
