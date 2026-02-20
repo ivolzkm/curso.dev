32f7d0c39d116b3419f16ac9491565804822de4c
ivolzkm <ivolzkmjr1326@gmail.com>
2026-02-20 09:32:49 -0300

    makes database.js more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,28 +1,26 @@
-//infra/database.js
-
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
-});
+import { Client } from "pg";
 
 async function query(queryObject) {
-  try {
-    const result = await pool.query(queryObject);
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  try { 
+    await client.connect();
+    const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
     throw error;
+  } finally {
+    await client.end();
   }
 }
+
 export default {
   query: query,
 };
